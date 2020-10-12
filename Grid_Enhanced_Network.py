ca9f117ec5b8809a5a09c16c56dfe84e6ed221d1
AnastasisKratsios <anastasis.kratsios@math.ethz.ch>
2020-10-12 19:09:06 +0200

    results in--done

--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -1,3 +1,6 @@
+trial_run = False
+# This one is with larger height
+
 # This file contains the hyper-parameter grids used to train the imprinted-tree nets.
 
 #----------------------#
@@ -50,7 +53,7 @@ else:
     # Training Parameters
     #----------------------#
     # Number of Jobs (Cores to use)
-    n_jobs = 4
+    n_jobs = 70
     # Number of Random CV Draws
     n_iter = 50
     n_iter_trees = 50
@@ -63,16 +66,16 @@ else:
     param_grid_Vanilla_Nets = {'batch_size': [16,32,64],
                         'epochs': [200, 400, 800, 1000, 1200, 1400],
                           'learning_rate': [0.0001,0.0005,0.005],
-                          'height': [100,200, 400],
-                           'depth': [1,2,3],
+                          'height': [100,200, 400, 600, 800],
+                           'depth': [1,2],
                           'input_dim':[15],
                            'output_dim':[1]}
     # Random Forest Grid
     #--------------------#
-    Rand_Forest_Grid = {'learning_rate': [0.1, 0.05, 0.02, 0.01, 0.01],
-                        'max_depth': [4, 6, 10, 50, 100],
-                        'min_samples_leaf': [1, 3, 5, 9, 17, 50, 100, 150, 200],
-                       'n_estimators': [100, 200, 500, 1000]
+    Rand_Forest_Grid = {'learning_rate': [0.0001,0.0005,0.005, 0.01],
+                        'max_depth': [1,2,3,4,5, 7, 10, 25, 50, 75,100, 150, 200, 300, 500],
+                        'min_samples_leaf': [1,2,3,4, 5, 9, 17, 20,50,75, 100],
+                       'n_estimators': [5, 10, 25, 50, 100, 200, 250]
                        }
                        
 
