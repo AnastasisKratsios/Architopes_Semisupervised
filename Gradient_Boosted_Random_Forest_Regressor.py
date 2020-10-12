6f0c2eda87fa0af25b80d84e8778b5beaaa2f2e6
AnastasisKratsios <anastasis.kratsios@math.ethz.ch>
2020-10-12 10:06:31 +0200

    done

--- Gradient_Boosted_Random_Forest_Regressor.py
+++ Gradient_Boosted_Random_Forest_Regressor.py
@@ -25,53 +25,53 @@ import time
 Gradient_boosted_Random_forest_time_Begin = time.time()
 
 # Compute/Load Gradient-boosted regression trees
-if os.path.exists('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl'): 
-#------------------------------------------------------------------------------------------#
-    # Alert User: Model (non-)Existence
-    print("Gradient Boosted Tree Found: Loaded!")
-    # Load Pickled Model
-    random_forest_trained = pickle.load(open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl', 'rb'))
-else:
-    # Alert User: Model (non-)Existence
-    print("No Gradient Boosted Tree Found: Training Model")
+# if os.path.exists('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl'): 
+# #------------------------------------------------------------------------------------------#
+#     # Alert User: Model (non-)Existence
+#     print("Gradient Boosted Tree Found: Loaded!")
+#     # Load Pickled Model
+#     random_forest_trained = pickle.load(open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl', 'rb'))
+# else:
+#     # Alert User: Model (non-)Existence
+#     print("No Gradient Boosted Tree Found: Training Model")
 
-    # Run Random Forest Util
-    rand_forest_model_grad_boosted = GradientBoostingRegressor()
+# Run Random Forest Util
+rand_forest_model_grad_boosted = GradientBoostingRegressor()
 
-    # Grid-Search CV
-    Random_Forest_GridSearch = RandomizedSearchCV(estimator = rand_forest_model_grad_boosted,
-                       n_iter=n_iter_trees,
-                       cv=KFold(CV_folds, random_state=2020, shuffle=True),
-                       param_distributions=Rand_Forest_Grid,
-                       return_train_score=True,
-                       random_state=2020,
-                       verbose=10,
-                       n_jobs=n_jobs)
-    random_forest_trained = Random_Forest_GridSearch.fit(X_train,y_train)
-    random_forest_trained = random_forest_trained.best_estimator_
-    
-    #--------------------------------------------------#
-    # Write: Model, Results, and Best Hyper-Parameters #
-    #--------------------------------------------------#
-    
-    # Save Model
-    pickle.dump(random_forest_trained, open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl','wb'))
+# Grid-Search CV
+Random_Forest_GridSearch = RandomizedSearchCV(estimator = rand_forest_model_grad_boosted,
+                   n_iter=n_iter_trees,
+                   cv=KFold(CV_folds, random_state=2020, shuffle=True),
+                   param_distributions=Rand_Forest_Grid,
+                   return_train_score=True,
+                   random_state=2020,
+                   verbose=10,
+                   n_jobs=n_jobs)
+random_forest_trained = Random_Forest_GridSearch.fit(X_train,y_train)
+random_forest_trained = random_forest_trained.best_estimator_
 
-    # Save Readings
-    cur_path = os.path.expanduser('./outputs/tables/best_params_Gradient_Boosted_Tree.txt')
-    with open(cur_path, "w") as f:
-        f.write(str(Random_Forest_GridSearch.best_params_))
+#--------------------------------------------------#
+# Write: Model, Results, and Best Hyper-Parameters #
+#--------------------------------------------------#
 
-    best_params_table_tree = pd.DataFrame({'N Estimators': [Random_Forest_GridSearch.best_params_['n_estimators']],
-                                        'Min Samples Leaf': [Random_Forest_GridSearch.best_params_['min_samples_leaf']],
-                                        'Learning Rate': [Random_Forest_GridSearch.best_params_['learning_rate']],
-                                        'Max Depth': [Random_Forest_GridSearch.best_params_['max_depth']],
-                                        })
-    best_params_table_tree.to_latex('./outputs/tables/Best_params_table_Gradient_Boosted_Tree.txt')
+# Save Model
+# pickle.dump(random_forest_trained, open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl','wb'))
+
+# Save Readings
+cur_path = os.path.expanduser('./outputs/tables/best_params_Gradient_Boosted_Tree.txt')
+with open(cur_path, "w") as f:
+    f.write(str(Random_Forest_GridSearch.best_params_))
+
+best_params_table_tree = pd.DataFrame({'N Estimators': [Random_Forest_GridSearch.best_params_['n_estimators']],
+                                    'Min Samples Leaf': [Random_Forest_GridSearch.best_params_['min_samples_leaf']],
+                                    'Learning Rate': [Random_Forest_GridSearch.best_params_['learning_rate']],
+                                    'Max Depth': [Random_Forest_GridSearch.best_params_['max_depth']],
+                                    })
+best_params_table_tree.to_latex('./outputs/tables/Best_params_table_Gradient_Boosted_Tree.txt')
 
 
-    # Update User
-    print("Gradient Boosted Trees Model - Done CV!")
+# Update User
+print("Gradient Boosted Trees Model - Done CV!")
 
 
 # #### Make Prediction(s)
--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -1,15 +1,5 @@
 # This file contains the hyper-parameter grids used to train the imprinted-tree nets.
 
-# Training Parameters
-#----------------------#
-# Number of Jobs (Cores to use)
-n_jobs = 4
-# Number of Random CV Draws
-n_iter = 1
-n_iter_trees = 1#20
-# Number of CV Folds
-CV_folds = 4
-
 #----------------------#
 ########################
 # Hyperparameter Grids #
@@ -25,14 +15,51 @@ N_Features_Search_Space_Dimension = 10**4
 # Hyperparameter Grid (Readout)
 #------------------------------#
 if trial_run == True:
+
+    
+    # Training Parameters
+    #----------------------#
+    # Number of Jobs (Cores to use)
+    n_jobs = 4
+    # Number of Random CV Draws
+    n_iter = 1
+    n_iter_trees = 1#20
+    # Number of CV Folds
+    CV_folds = 4
+
+    
+    # Model Parameters
+    #------------------#
     param_grid_Vanilla_Nets = {'batch_size': [8],
-                    'epochs': [1200],
+                    'epochs': [2],
                       'learning_rate': [0.14],
-                      'height': [200],
+                      'height': [2],
                        'depth': [1],
                       'input_dim':[15],
                        'output_dim':[1]}
+    # Random Forest Grid
+    #--------------------#
+    Rand_Forest_Grid = {'learning_rate': [0.1],
+                        'max_depth': [2],
+                        'min_samples_leaf': [1],
+                       'n_estimators': [10],
+                       }
+    
 else:
+    
+    # Training Parameters
+    #----------------------#
+    # Number of Jobs (Cores to use)
+    n_jobs = 4
+    # Number of Random CV Draws
+    n_iter = 50
+    n_iter_trees = 50
+    # Number of CV Folds
+    CV_folds = 4
+    
+    
+    # Model Parameters
+    #------------------#
     param_grid_Vanilla_Nets = {'batch_size': [16,32,64],
                         'epochs': [200, 400, 800, 1000, 1200, 1400],
                           'learning_rate': [0.0001,0.0005,0.005],
@@ -40,11 +67,12 @@ else:
                            'depth': [1,2,3],
                           'input_dim':[15],
                            'output_dim':[1]}
+    # Random Forest Grid
+    #--------------------#
+    Rand_Forest_Grid = {'learning_rate': [0.1, 0.05, 0.02, 0.01, 0.01],
+                        'max_depth': [4, 6, 10, 50, 100],
+                        'min_samples_leaf': [1, 3, 5, 9, 17, 50, 100, 150, 200],
+                       'n_estimators': [100, 200, 500, 1000]
+                       }
                        
-# Random Forest Grid
-#--------------------#
-Rand_Forest_Grid = {'learning_rate': [0.1],#, 0.05, 0.02, 0.01, 0.01],
-                    'max_depth': [40],#, 6, 10,50, 100],
-                    'min_samples_leaf': [17],#, 5, 9, 17, 50, 100, 150, 200],
-                   'n_estimators': [1000],#00, 200, 500, 1000]
-                   }
+
