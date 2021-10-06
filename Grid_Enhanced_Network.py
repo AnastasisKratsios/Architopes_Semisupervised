d2348f0a6c4602d41821e2f2c138e0100fa1c95b
Anastasis Kratsios <57273304+AnastasisKratsios@users.noreply.github.com>
2021-10-06 16:10:46 +0200

    Added Mean Subnetwork Widths

--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -1,6 +1,6 @@
 # Which financial dataset do you want to consider (NB this meta-parameter does not impact the non-financial architopes module)
 # Options: AAPL, SnP, or crypto (or Motivational_Example for DEMO version!)
-Option_Function = "Motivational"
+Option_Function = "Motivational_Example"
 
 # Is this a trial run (to test hardware?)
 trial_run = True
@@ -34,7 +34,7 @@ if trial_run == True:
     # Model Parameters
     #------------------#
     param_grid_Vanilla_Nets = {'batch_size': [16],
-                    'epochs': [2],
+                    'epochs': [400],
                       'learning_rate': [0.0001],
                       'height': [400],
                        'depth': [2],
