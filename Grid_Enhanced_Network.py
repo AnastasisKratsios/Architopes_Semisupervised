23eae519a3e1e1268d028144c106a6a72250939b
Anastasis Kratsios <57273304+AnastasisKratsios@users.noreply.github.com>
2021-10-10 04:49:35 +0200

    fixed bug with randomized nns

--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -34,18 +34,18 @@ if trial_run == True:
     # Model Parameters
     #------------------#
     param_grid_Vanilla_Nets = {'batch_size': [16],
-                    'epochs': [200],
+                    'epochs': [20],
                       'learning_rate': [0.01],
-                      'height': [800],
-                      'depth': [2],
+                      'height': [20],
+                      'depth': [1],
                       'input_dim':[1],
                       'output_dim':[1]}
 
     param_grid_Deep_Classifier = {'batch_size': [16],
-                        'epochs': [600],
+                        'epochs': [6],
                         'learning_rate': [0.000001],
-                        'height': [200],
-                        'depth': [4],
+                        'height': [2],
+                        'depth': [1],
                         'input_dim':[1],
                         'output_dim':[1]}
 
--- Helper_Functions.py
+++ Helper_Functions.py
@@ -193,12 +193,13 @@ from scipy.sparse import random as randsp
 from scipy.sparse import csr_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn import linear_model
-def build_ffNN_random(X_train_in,X_test_in,y_train_in,param_grid_in):
+def build_ffNN_random(X_train_in,X_train_in_full,X_test_in,y_train_in,param_grid_in):
     # Initializations
     ## Model
     clf = linear_model.Lasso(alpha=0.1)
     ## Features to Randomize
     X_train_rand_features = X_train_in.to_numpy()
+    X_train_full_rand_features = X_train_in_full.to_numpy()
     X_test_rand_features = X_test_in.to_numpy()
     N_Random_Features = param_grid_in['height'][0]
     N_Random_Features_Depth = param_grid_in['depth'][0]
@@ -209,11 +210,16 @@ def build_ffNN_random(X_train_in,X_test_in,y_train_in,param_grid_in):
         biases_rand = np.random.uniform(low=-.5,high=.5,size = N_Random_Features)
         ### Apply Random (hidden) Weights
         X_train_rand_features = sparse.csr_matrix.dot(X_train_rand_features,Weights_rand)
+        X_train_full_rand_features = sparse.csr_matrix.dot(X_train_full_rand_features,Weights_rand)
         #### Apply Random (hidden) Biases
         X_train_rand_features = X_train_rand_features + biases_rand
         X_train_rand_features = np.sin(X_train_rand_features)
+        #####
+        X_train_full_rand_features = X_train_full_rand_features + biases_rand
+        X_train_full_rand_features = np.sin(X_train_full_rand_features)
         #### Compress
         X_train_rand_features = sparse.csr_matrix(X_train_rand_features)
+        X_train_full_rand_features = sparse.csr_matrix(X_train_full_rand_features)
 
         #------# Test #-------------#
         #### Apply Random (hidden) Weights
@@ -223,16 +229,18 @@ def build_ffNN_random(X_train_in,X_test_in,y_train_in,param_grid_in):
         #### Apply Discontinuous (Step) Activation function
         X_test_rand_features = np.sin(X_test_rand_features)
         #### Compress
-        X_test_rand_features = sparse.csr_matrix(X_train_rand_features)
+        X_test_rand_features = sparse.csr_matrix(X_test_rand_features)
     # Get regressor 
     clf.fit(X_train_rand_features,y_train_in)
-    y_hat_train = clf.predict(X_train_rand_features)
+    y_hat_train = clf.predict(X_train_full_rand_features)
     ### Predict
     y_hat_test = clf.predict(X_test_rand_features)
     
     # Count Parameters
     N_parameters = (N_Random_Features * N_Random_Features_Depth) + N_Random_Features*(param_grid_in['input_dim'][0]+param_grid_in['output_dim'][0])
     # Return Computations
+    y_hat_train = y_hat_train.reshape(-1,param_grid_in['output_dim'][0])
+    y_hat_test = y_hat_test.reshape(-1,param_grid_in['output_dim'][0])
     return y_hat_train, y_hat_test, N_parameters
 
 
