23defe51344344856e03d3b6b82d088441bccbe8
Anastasis Kratsios <57273304+AnastasisKratsios@users.noreply.github.com>
2021-10-11 11:18:51 +0200

    Updated Facts Generation
    Co-Authored-By: Behnoosh Zamanlooy <61875950+bzamanlooy@users.noreply.github.com>

--- Gradient_Boosted_Random_Forest_Regressor.py
+++ Gradient_Boosted_Random_Forest_Regressor.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Gradient-Boosted Random Forest Regressor
-# ---
-
-# ### Reload Initialization(s)
-
-# In[15]:
-
-
 # Load Packages/Modules
 exec(open('Init_Dump.py').read())
 # Load Hyper-parameter Grid
@@ -16,28 +5,10 @@ exec(open('Grid_Enhanced_Network.py').read())
 # Import Time (for some reason must be done internally?)
 import time
 
-
-# ### Build Random Forest Regressor with Gradient Boosting
-
-# In[16]:
-
 # Initialize time-elapsed computation
 Gradient_boosted_Random_forest_time_Begin = time.time()
-
-# Compute/Load Gradient-boosted regression trees
-# if os.path.exists('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl'): 
-# #------------------------------------------------------------------------------------------#
-#     # Alert User: Model (non-)Existence
-#     print("Gradient Boosted Tree Found: Loaded!")
-#     # Load Pickled Model
-#     random_forest_trained = pickle.load(open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl', 'rb'))
-# else:
-#     # Alert User: Model (non-)Existence
-#     print("No Gradient Boosted Tree Found: Training Model")
-
 # Run Random Forest Util
 rand_forest_model_grad_boosted = GradientBoostingRegressor()
-
 # Grid-Search CV
 Random_Forest_GridSearch = RandomizedSearchCV(estimator = rand_forest_model_grad_boosted,
                    n_iter=n_iter_trees,
@@ -49,14 +20,9 @@ Random_Forest_GridSearch = RandomizedSearchCV(estimator = rand_forest_model_grad
                    n_jobs=n_jobs)
 random_forest_trained = Random_Forest_GridSearch.fit(X_train,y_train)
 random_forest_trained = random_forest_trained.best_estimator_
-
 #--------------------------------------------------#
 # Write: Model, Results, and Best Hyper-Parameters #
 #--------------------------------------------------#
-
-# Save Model
-# pickle.dump(random_forest_trained, open('./outputs/models/Gradient_Boosted_Tree/Gradient_Boosted_Tree_Best.pkl','wb'))
-
 # Save Readings
 cur_path = os.path.expanduser('./outputs/tables/best_params_Gradient_Boosted_Tree.txt')
 with open(cur_path, "w") as f:
@@ -68,24 +34,12 @@ best_params_table_tree = pd.DataFrame({'N Estimators': [Random_Forest_GridSearch
                                     'Max Depth': [Random_Forest_GridSearch.best_params_['max_depth']],
                                     })
 best_params_table_tree.to_latex('./outputs/tables/Best_params_table_Gradient_Boosted_Tree.txt')
-
-
 # Update User
 print("Gradient Boosted Trees Model - Done CV!")
-
-
-# #### Make Prediction(s)
-
-# In[17]:
-
-
 y_train_hat_random_forest_Gradient_boosting = random_forest_trained.predict(X_train)
 y_test_hat_random_forest_Gradient_boosting = random_forest_trained.predict(X_test)
-
-
 # Evaluate time-elapsed computation
 Gradient_boosted_Random_forest_time = time.time() - Gradient_boosted_Random_forest_time_Begin
-
 # Count Number of Parameters in Random Forest Regressor
 N_tot_params_per_tree = [ (x[0].tree_.node_count)*random_forest_trained.n_features_ for x in random_forest_trained.estimators_]
 N_tot_params_in_forest = sum(N_tot_params_per_tree)
@@ -93,17 +47,7 @@ N_tot_params_in_forest = sum(N_tot_params_per_tree)
 # Note to potential code-reader:
 # Thanks Philip Casgain for the counter! :D
 #---------------------------------------------#
-
-# Update User #
-#--------------#
 print("Random Forest Regressor uses: "+str(N_tot_params_in_forest)+" parameters.")
-
-
-# #### Write Prediction(s)
-
-# In[18]:
-
-
 # Compute Peformance
 Gradient_boosted_tree = reporter(y_train_hat_in=y_train_hat_random_forest_Gradient_boosting,
                                     y_test_hat_in=y_test_hat_random_forest_Gradient_boosting,
@@ -111,13 +55,8 @@ Gradient_boosted_tree = reporter(y_train_hat_in=y_train_hat_random_forest_Gradie
                                     y_test_in=y_test)
 # Write Performance
 Gradient_boosted_tree.to_latex((results_tables_path+"Gradient_Boosted_Tree_Performance.tex"))
-
-
-
 # Update User
 print(Gradient_boosted_tree)
-
-
 # ---
 # # Fin
 # ---
--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -26,7 +26,7 @@ if trial_run == True:
     n_jobs = 4
     # Number of Random CV Draws
     n_iter = 1
-    n_iter_trees = 10
+    n_iter_trees = 5
     # Number of CV Folds
     CV_folds = 2
 
@@ -35,7 +35,7 @@ if trial_run == True:
     #------------------#
     param_grid_FFNNs = {'batch_size': [16],
                     'epochs': [100],
-                      'learning_rate': [0.00001],
+                      'learning_rate': [0.0001],
                       'height': [200],
                       'depth': [2],
                       'input_dim':[1],
@@ -43,7 +43,7 @@ if trial_run == True:
     param_grid_Vanilla_Nets = {'batch_size': [16],
                     'epochs': [100],
                       'learning_rate': [0.00001],
-                      'height': [200],
+                      'height': [400],
                       'depth': [2],
                       'input_dim':[1],
                       'output_dim':[1]}
@@ -51,7 +51,7 @@ if trial_run == True:
     param_grid_Deep_Classifier = {'batch_size': [16],
                         'epochs': [200],
                         'learning_rate': [0.00001],
-                        'height': [2000],
+                        'height': [1500],
                         'depth': [1],
                         'input_dim':[1],
                         'output_dim':[1]}
@@ -59,10 +59,10 @@ if trial_run == True:
                        
     # Random Forest Grid
     #--------------------#
-    Rand_Forest_Grid = {'learning_rate': [0.01,0.001],
-                        'max_depth': [2,3,4],
-                        'min_samples_leaf': [2,3,5],
-                       'n_estimators': [200],
+    Rand_Forest_Grid = {'learning_rate': [0.0001],
+                        'max_depth': [2,3],
+                        'min_samples_leaf': [5,6,8],
+                       'n_estimators': [100],
                        }
     
 else:
